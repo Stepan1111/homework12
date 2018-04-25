6ac2ecdb998ac2f1abbc78cb5d143d279f2c8c60
Stepan <qwesdfq_95@ukr.net>
2018-04-25 00:10:35 +0000

    q

---
+++ homework/weather.js
@@ -0,0 +1,45 @@
+const request = require('request');
+const argv = require('yargs').argv
+const fs = require('fs');
+
+const geocodingKey = 'AIzaSyBWT1XRcwa9QqxKI90bZBUnLcxwDtwJ_Ms';
+const darkSkyKey = '4cd0b2586d83d2497df00848b8996ff4';
+
+const geocodingUrl =
+    "https://maps.googleapis.com/maps/api/geocode/json?address=" + argv.city + "&key=" + geocodingKey;
+
+request.get(geocodingUrl, (error, response, body) => {
+    let geocodingJson = JSON.parse(body);
+    if (geocodingJson.status !== 'ZERO_RESULTS') {
+        let lat = geocodingJson.results[0].geometry.location.lat;
+        let lng = geocodingJson.results[0].geometry.location.lng;
+        let darkSkyUrl = "https://api.darksky.net/forecast/" + darkSkyKey + "/" + lat + "," + lng;
+        request.get(darkSkyUrl, (error, response, body) => {
+            let darkSkyJson = JSON.parse(body);
+            console.log(
+                `temperature: ${darkSkyJson.currently.temperature} -`,
+                `icon: ${darkSkyJson.currently.icon} -`,
+                `humidity: ${darkSkyJson.currently.humidity}`,
+                `wind speed: ${darkSkyJson.currently.windSpeed}`
+            );
+            let weather = 'Now in ' +
+                geocodingJson.results[0].address_components[0].long_name +
+                ' ' + darkSkyJson.currently.icon +
+                ' with temperature: ' +
+                darkSkyJson.currently.temperature +
+                ', humidity: ' +
+                darkSkyJson.currently.humidity +
+                ', and wind speed: ' +
+                darkSkyJson.currently.windSpeed + ' .';
+            fs.writeFile("./weather.txt", weather, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+
+        });
+    }
+    else {
+        console.log("Sorry, such a city does not exist")
+    }
+});
